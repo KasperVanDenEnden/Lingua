eb48cab10bf96bbc10c0fd665737ae010f0ff15c
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2025-01-19 15:23:51 +0100

    Added @nestjs/swagger to api's

--- apps/rcmnd-api/src/main.ts
+++ apps/rcmnd-api/src/main.ts
@@ -6,10 +6,21 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
+  const config = new DocumentBuilder()
+    .setTitle('Lingua Data API')
+    .setDescription('Lingua swagger documentation')
+    .setVersion('1.0')
+    .addTag('meal')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, documentFactory);
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3100;
   await app.listen(port);

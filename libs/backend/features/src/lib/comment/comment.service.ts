45fdd1b3abd435b48885c81d744873596dfebf37
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2025-02-05 16:13:57 +0100

    Get all assistant/class combination & add and remove assistant from class (#13)

---
+++ libs/backend/features/src/lib/comment/comment.service.ts
@@ -0,0 +1,83 @@
+import { IComment, Id, } from '@lingua/api';
+import {  Class, ClassDocument } from '@lingua/schemas';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+
+@Injectable()
+export class CommentService {
+     private TAG = 'CommentService';
+      constructor(@InjectModel(Class.name) private classModel: Model<ClassDocument>) {}
+    
+    //   async getAll(): Promise<IComment[]> {
+    //     Logger.log('getAll', this.TAG);
+    //     return await this.classModel.find();
+    //   }
+    
+    //   async getOne(id: Id): Promise<IComment> {
+    //     Logger.log('getOne', this.TAG);
+    
+    //     const classInstance = await this.classModel.findById(id).exec();
+    
+    //     if (!classInstance) throw new HttpException('Comment not found', HttpStatus.NOT_FOUND);
+    
+    //     return ;
+    //   }
+    
+      async create(body: IComment): Promise<IComment> {
+        Logger.log('create', this.TAG);
+
+        const updatedClass = await this.classModel.findByIdAndUpdate({ _id: new Types.ObjectId(body.class)},
+            { 
+                $push : {
+                    comments: body
+                }
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )    
+        
+        if (!updatedClass) throw new HttpException('Class not found', HttpStatus.NOT_FOUND);
+    
+        return body;
+      }
+    
+    //   async update(id: Id, changes: IUpdateComment): Promise<IComment> {
+    //     Logger.log('update', this.TAG);
+    
+    //     const updatedComment = await this.classModel.findByIdAndUpdate(
+    //       { _id: id },
+    //       changes,
+    //       { new: true }
+    //     );
+    
+    //     if (!updatedComment)
+    //       throw new HttpException('Comment not found', HttpStatus.NOT_FOUND);
+    
+    //     return updatedComment;
+    //   }
+    
+      async delete(id: Id, classId: Id) {
+        Logger.log('delete', this.TAG);
+    
+        const updatedClass = await this.classModel.findByIdAndUpdate(
+            classId,
+            { 
+                $pull: { 
+                    comments: { _id: id } 
+                } 
+            },
+            { 
+                new: true,  // Return the updated document
+                runValidators: true 
+            }
+        );
+    
+        if (!updatedClass)
+            throw new HttpException('Class not found', HttpStatus.NOT_FOUND);
+    
+        return ;
+      }
+}

cffb65011a346919a01d336c25cce9bd2176dc35
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2025-02-06 14:35:37 +0100

    Register,unregister, get studentlist by class, get registrations aggregate

--- libs/backend/features/src/lib/student/class-registration.controller.ts
+++ libs/backend/features/src/lib/student/class-registration.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/role-auth.guard';
+import { BodyObjectIdsPipe, IClassRegistration, Id, IUser, Role, stringObjectIdPipe } from '@lingua/api';
+import { Roles } from '../auth/decorators/role.decorator';
+import { ClassRegistrationService } from './class-registration.service';
 
 @Controller('class-registration')
-export class ClassRegistrationController {}
+@UseGuards(JwtAuthGuard)
+export class ClassRegistrationController {
+    private TAG = 'ClassRegistrationController'
+
+    constructor(private classRegistrationService: ClassRegistrationService) {}
+
+    @UseGuards(RolesGuard)
+    @Roles(Role.Teacher, Role.Admin)
+    @Get()
+    async getRegistrations(): Promise<IClassRegistration[]> {
+        Logger.log('getRegistrations', this.TAG);
+        return await this.classRegistrationService.getRegistrations()
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles(Role.Teacher, Role.Admin)
+    @Get(':id')
+    async getRegisteredStudents(@Param('id', stringObjectIdPipe) id:Id): Promise<IUser[]> {
+        Logger.log('getRegisteredStudents', this.TAG);
+        return await this.classRegistrationService.getRegisteredStudents(id);
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles(Role.Student, Role.Admin)
+    @Post()
+    async register(@Body(BodyObjectIdsPipe) body: IClassRegistration): Promise<IClassRegistration> {
+        Logger.log('register', this.TAG)
+        return await this.classRegistrationService.register(body);
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles(Role.Student, Role.Admin)
+    @Put(':id')
+    async unregister(@Param('id', stringObjectIdPipe) id:Id): Promise<IClassRegistration> {
+        Logger.log('unregister', this.TAG)
+        return await this.classRegistrationService.unregister(id);
+    }
+
+    @UseGuards(RolesGuard)
+    @Roles(Role.Admin)
+    @Get(':id')
+    async delete(@Param('id', stringObjectIdPipe) id:Id): Promise<IClassRegistration> {
+        Logger.log('delete', this.TAG);
+        return await this.classRegistrationService.delete(id);
+    }
+}
--- libs/backend/features/src/lib/student/class-registration.service.ts
+++ libs/backend/features/src/lib/student/class-registration.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { IClassRegistration, Id, IUser } from '@lingua/api';
+import { ClassRegistration, ClassRegistrationDocument } from '@lingua/schemas';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ClassRegistrationService {}
+export class ClassRegistrationService {
+    private TAG = 'ClassRegistrationService';
+    
+    constructor(@InjectModel(ClassRegistration.name) private classRegistrationModel: Model<ClassRegistrationDocument>) {}
+
+    async getRegistrations(): Promise<IClassRegistration[]> {
+        Logger.log('getRegistrations', this.TAG);
+
+        const registrations = await this.classRegistrationModel.aggregate([
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'student',
+                    foreignField: '_id',
+                    as: 'studentDetails'    
+                }
+            },
+            {
+                $lookup: {
+                    from: 'classes',
+                    localField: 'class',
+                    foreignField: '_id',
+                    as: 'classDetails'    
+                }
+            },
+            {
+                $unwind: { path: '$studentDetails', preserveNullAndEmptyArrays: true }  // Dit haalt het student detail uit het array
+            },
+            {
+                $unwind: { path: '$classDetails', preserveNullAndEmptyArrays: true }    // Dit haalt het class detail uit het array
+            }
+        ])
+
+        return registrations;
+    }
+
+    async getRegisteredStudents(classId: Id): Promise<IUser[]> {
+        Logger.log(`Fetching registered students for class ${classId}`, this.TAG);
+
+        const registrations = await this.classRegistrationModel.find({
+            class: classId,
+            unregisteredAt: { $exists: false },  
+        })
+        .populate('student') 
+        .lean(); 
+    
+        return registrations.map((reg: any) => reg.student); 
+    }
+
+    async register(body:IClassRegistration): Promise<IClassRegistration>{
+        Logger.log('register', this.TAG);
+
+        const existingRecord = await this.classRegistrationModel.findOne({
+            class: body.class,
+            student: body.student,
+            unregisteredAt: { $exists: false }
+        })
+
+        if (existingRecord) throw new HttpException('Student already registered', HttpStatus.BAD_REQUEST);
+        
+        return await this.classRegistrationModel.create(body);
+    }
+
+    async unregister(id:Id): Promise<IClassRegistration> {
+        Logger.log('unregister', this.TAG);
+
+        const updatedClassRegistration = await this.classRegistrationModel.findByIdAndUpdate(
+            { _id: id},
+            { unregisteredAt: Date.now()},
+            { 
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!updatedClassRegistration) throw new HttpException('Class registration not found', HttpStatus.NOT_FOUND);
+
+        return updatedClassRegistration;
+    }
+
+    async delete(id:Id): Promise<IClassRegistration> {
+        Logger.log('delete', this.TAG);
+
+        const deletedRegistration = await this.classRegistrationModel.findByIdAndDelete({id});
+
+        if(!deletedRegistration) throw new HttpException('Class registration not found', HttpStatus.NOT_FOUND);
+
+        return deletedRegistration;
+    }
+}

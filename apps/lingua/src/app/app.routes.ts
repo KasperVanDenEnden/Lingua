bb88c6aacb472c11d2f78c95e41147834351a31f
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2025-02-20 21:54:05 +0100

    RolesGuard routes

--- apps/lingua/src/app/app.routes.ts
+++ apps/lingua/src/app/app.routes.ts
@@ -13,42 +13,44 @@ import {
 } from '@lingua/pages';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { NgModule } from '@angular/core';
-import { AuthGuard } from '@lingua/pages';
+import { AuthGuard, RolesGuard } from '@lingua/pages';
+import { CommonComponent } from '@lingua/common';
 
 export const appRoutes: Route[] = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'pages', component: PagesComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, RolesGuard],
   },
-  { path: 'locations', component: LocationListComponent, canActivate: [AuthGuard], children: [
-    { path: 'new',pathMatch:'full', component: LocationFormComponent },
+  { path: 'locations', component: LocationListComponent, canActivate: [AuthGuard, RolesGuard], children: [
+    { path: 'new',pathMatch:'full', component: LocationFormComponent, canActivate: [RolesGuard], data: { role: 'teacher'} },
   ] },
   {
     path: 'locations/:id',
     canActivate: [AuthGuard],
     component: LocationDetailComponent,
     children: [
-      { path: 'edit', pathMatch:'full', component: LocationFormComponent },
+      { path: 'edit', pathMatch:'full', component: LocationFormComponent, canActivate: [RolesGuard], data: { role: 'teacher'} },
     ],
   },
   { path: 'rooms', component: RoomListComponent, canActivate: [AuthGuard], children: [
-    { path: 'new', pathMatch: 'full', component: RoomFormComponent }
+    { path: 'new', pathMatch: 'full', component: RoomFormComponent, canActivate: [RolesGuard], data: { role: 'teacher'} }
   ] },
   {
     path: 'rooms/:id',
     canActivate: [AuthGuard],
     component: RoomDetailComponent,
     children: [
-      { path: 'edit', pathMatch:'full', component: RoomFormComponent },
+      { path: 'edit', pathMatch:'full', component: RoomFormComponent, canActivate: [RolesGuard], data: { role: 'teacher'} },
     ]
   },
   { path: 'classes', component: NxWelcomeComponent, canActivate: [AuthGuard] },
   { path: 'lessons', component: NxWelcomeComponent, canActivate: [AuthGuard] },
+  { path: 'pages', component: PagesComponent},
+  { path: 'access-denied', component: CommonComponent},
 ];
 
 @NgModule({
--- libs/lingua/pages/src/lib/auth/auth.guard.ts
+++ libs/lingua/pages/src/lib/auth/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.authService.isAuthenticated()) {
+    if (this.authService.isAuthenticated() && this.authService.validateToken()) {
       return true;
     }
 

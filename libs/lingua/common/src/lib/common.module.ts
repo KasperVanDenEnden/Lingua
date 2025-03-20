cdc8faef4bf051fe3a835062cdddfbb94ff85f0b
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2025-03-20 11:51:30 +0100

    Develop (#55)
    * Fix failing tests
    
    * Dto/tests (#24)
    
    * tests class registration dto
    
    * tests login dto
    
    * tests register dto
    
    * class registration schema tests (#26)
    
    * Refactor comment to review
    
    * Angular/auth (#29)
    
    * Pages library
    
    * tailwindcss
    
    * Navbar template added
    
    * Prefix components to lingua
    
    * Fontawesome
    
    * User can log in through form, returnUrl when redirect to login
    
    * register student and redirect to dashboard on with automatic login
    
    * Header component with shown currentUser
    
    * List/detail/form implemented (#30)
    
    * Feature/rooms (#31)
    
    * Overview
    
    * Form
    
    * Detail
    
    * RolesGuard routes (#33)
    
    * Fix routes file
    
    * Feature/classes (#36)
    
    * List page
    
    * Dedail and form pages
    
    * assign en remove assistants (#38)
    
    * Added properties to locatin and rooms. updated dto's schemas and code that relies on those (#39)
    
    * Nav update
    
    * Feature/lessons (#40)
    
    * mobile menu color styling
    
    * List and Form pages
    
    * Rooms slugs and form styling better width and inputs w-full
    
    * Detail page
    
    * Small lesson form improvement
    
    * Class to course (#42)
    
    * Backend refactored
    
    * Frontend refactored
    
    * lesson form fix
    
    * Feature/delete (#43)
    
    * Services library
    
    * Removed unused declarations in components.ts
    
    * Lesson list delete
    
    * ngx-toastr
    
    * Cleanup imports components
    
    * Delete on all entity list and detail pages
    
    * Lesson model has status, form updated with status, title,description inputs (#44)
    
    * Feature/user profile pages (#45)
    
    * USer detail page
    
    * Logout confirmation modal
    
    * Password change form
    
    * User form, for profile edit by all users and create for only admin with copy to clipboard toastr
    
    * User-list component
    
    * User overview delete only when admin
    
    * New user button only when admin
    
    * Removed test files for components in library lingua (#48)
    
    * Fixed errors regarding budget, imports (#52)
    
    * Nx/build error fixes (#53)
    
    * Fixed errors regarding budget, imports
    
    * Removed expects from app.component.spec.ts since old
    
    * Nx/build error fixes (#54)
    
    * Fixed errors regarding budget, imports
    
    * Removed expects from app.component.spec.ts since old
    
    * Removed app.component.spec.ts
    
    * App.cy.ts from lingua-e2e is a dummy test

--- libs/lingua/common/src/lib/common.module.ts
+++ libs/lingua/common/src/lib/common.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
-import { FooterComponent, HeaderComponent, NavComponent } from "@lingua/common";
 import { CardComponent } from "@lingua/ui";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
     imports: [
